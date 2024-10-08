54f83ce3228a13a780e52ef39058c6f7412269ca
Kingcode <o2real@naver.com>
2024-10-08 22:41:45 +0900

    stock alert

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from twilio.rest import Client
 
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
@@ -8,6 +9,8 @@ NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 STOCK_API_KEY = "X77YKLDU0A6ONTZ7"
 NEWS_API_KEY = "ddca1dc01695456ca32a666ac592f305"
+TWILIO_ID = "asdfadsfaf"
+TWILIO_AUTH_TOKEN = "asdfdsfsafafdsf"
     ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 
@@ -33,11 +36,14 @@ print(day_before_yesterday_closing_price)
 
 #TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
 difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
-print(difference)
-
+up_down = None
+if difference > 0 :
+    up_down = "🔺"
+else:
+    up_down = "🔻"
 
 #TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
-diff_percent = (difference / float(yesterday_closing_price)) * 100
+diff_percent = round((difference / float(yesterday_closing_price)) * 100)
 print(diff_percent)
 
 #TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
@@ -46,22 +52,32 @@ print(diff_percent)
     # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
 
 #TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
-if diff_percent > 3 :
-    print("Get News")
-
-
+if abs(diff_percent) > 3 :
+    news_params = {
+        "apiKey": NEWS_API_KEY,
+        "qInTitle": COMPANY_NAME,
+    }
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+    articles = news_response.json()["articles"]
+    print(articles)
 #TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
-
+    three_articles = articles[:3]
+    print(three_articles)
 
     ## STEP 3: Use twilio.com/docs/sms/quickstart/python
     #to send a separate message with each article's title and description to your phone number. 
 
 #TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
-
+    formatted = [f"{STOCK_NAME}: {up_down}{diff_percent}%\nHeadline: {article['title']}. \nBrief: {article['description']}" for article in three_articles]
 #TODO 9. - Send each article as a separate message via Twilio. 
+    client = Client(TWILIO_ID, TWILIO_AUTH_TOKEN)
 
 
-
+    for article in formatted_articles:
+        message = client.messages.create(
+            body=article,
+            from_="5421312",
+            to="0107465321"
 #Optional TODO: Format the message like this: 
 """
 TSLA: 🔺2%
